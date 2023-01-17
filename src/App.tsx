630258cc21c1799f34092c60c3460ce6dc96b133
zodea <zd929560638@gmail.com>
2023-01-17 22:23:07 +0800

    style: :art: eslint tsx and json file

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,5 @@
 function App() {
-  return (
-    <div className="App">
-      this is the first React boilerplate App
-    </div>
-  )
+  return <div className="App">this is the first React boilerplate App</div>
 }
 
 export default App
-
