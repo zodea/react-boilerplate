5adae82d2b12e20af1f24b0a35b4f52d69ed84d1
zodea <zd929560638@gmail.com>
2023-01-17 22:21:57 +0800

    chore: :arrow_down: add visualizer config

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,21 @@
-import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import { visualizer } from 'rollup-plugin-visualizer'
+import { type Plugin, defineConfig } from 'vite'
 import EslintPlugin from 'vite-plugin-eslint'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), EslintPlugin()],
+  plugins: [
+    react(),
+    EslintPlugin(),
+    // 打包后展示分析图谱
+    visualizer({
+      filename: './node_modules/.cache/visualizer/stats.html',
+      open: true,
+      // @ts-ignore
+      gzipSize: true,
+      // @ts-ignore
+      brotliSize: true,
+    }) as unknown as Plugin,
+  ],
 })
